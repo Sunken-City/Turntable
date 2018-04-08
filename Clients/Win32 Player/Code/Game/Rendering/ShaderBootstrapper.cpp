db66bf6895662e64753875579fe0cf44367ccf3d
Anthony Cloudy <acloudy@mail.smu.edu>
2018-04-07 17:33:10 -0700

    Any shader toy shader that doesn't use the channels should work now!

--- Clients/Win32 Player/Code/Game/Rendering/ShaderBootstrapper.cpp
+++ Clients/Win32 Player/Code/Game/Rendering/ShaderBootstrapper.cpp
@@ -7,6 +7,10 @@
 #include "Engine/Math/Vector3.hpp"
 #include "Engine/Math/Vector4.hpp"
 #include "Engine/Time/Time.hpp"
+#include "Game/Audio/SongManager.hpp"
+#include "Game/Audio/Song.hpp"
+#include <chrono>
+#include <ctime>
 
 extern int WINDOW_PHYSICAL_WIDTH;
 extern int WINDOW_PHYSICAL_HEIGHT;
@@ -15,10 +19,14 @@ const char* ShaderBootstrapper::shaderHeader =
 "#version 410 core\n"
 "uniform vec3 iResolution;"
 "uniform float iTime;"
-"uniform float iFrame;"
 "uniform float iTimeDelta;"
+"uniform float iFrame;"
 "uniform float iFrameRate;"
+"uniform float iChannelTime[4];"
 "uniform vec4 iMouse;"
+"uniform vec4 iDate;"
+"uniform float iSampleRate;"
+"uniform vec3 iChannelResolution[4];"
 "out vec4 outColor;\n";
 
 const char* ShaderBootstrapper::mainFunction =
@@ -49,12 +57,24 @@ ShaderProgram* ShaderBootstrapper::compileShader(const char* vertexShaderPath, c
 //-----------------------------------------------------------------------------------
 void ShaderBootstrapper::initializeUniforms(ShaderProgram* program)
 {
+    float iChannelTime[4] = { 0.0f, 0.0f, 0.0f, 0.0f };
+    Vector3 iChannelResolution[4] = { Vector3::ZERO, Vector3::ZERO, Vector3::ZERO, Vector3::ZERO };
+    std::time_t timeNow = std::time(nullptr);
+    //Supressed because localtime_s isn't actually accessible because I don't know why try for yourself. >:I
+#pragma warning(suppress: 4996)
+    std::tm time = *std::localtime(&timeNow);
+
     program->SetVec3Uniform("iResolution", Vector3(static_cast<float>(WINDOW_PHYSICAL_WIDTH), static_cast<float>(WINDOW_PHYSICAL_HEIGHT), 1.0f));
     program->SetFloatUniform("iTime", static_cast<float>(GetCurrentTimeSeconds()));
     program->SetFloatUniform("iTimeDelta", 0.0f);
     program->SetFloatUniform("iFrame", 0.0f);
     program->SetFloatUniform("iFrameRate", GetCurrentFrameRate());
+    program->SetFloatUniform("iChannelTime", iChannelTime, 4);
     program->SetVec4Uniform("iMouse", Vector4(0.0f));
+    program->SetVec4Uniform("iDate", Vector4(static_cast<float>(time.tm_year - 1), static_cast<float>(time.tm_mon - 1), static_cast<float>(time.tm_mday), static_cast<float>(time.tm_sec))); //TODO: include fractional part of sec.
+    program->SetFloatUniform("iSampleRate", 0.0f);
+    program->SetVec3Uniform("iChannelResolution", iChannelResolution, 4);
+    //program->SetUniform("iChannelResolution", iChannelResolution, 4);
 }
 
 //-----------------------------------------------------------------------------------
@@ -69,9 +89,29 @@ void ShaderBootstrapper::updateUniforms(ShaderProgram* program, float deltaSecon
     currentPos.y = static_cast<float>(WINDOW_PHYSICAL_HEIGHT) - currentPos.y;
     dragPos.y = isClicking ? static_cast<float>(WINDOW_PHYSICAL_HEIGHT) - dragPos.y : -(static_cast<float>(WINDOW_PHYSICAL_HEIGHT) - (-dragPos.y));
 
+    //Calculate song channel time
+    float songTime = 0.0f;
+    float sampleRate = 0.0f;
+    if (SongManager::instance->IsPlaying())
+    {
+        songTime = static_cast<float>(AudioSystem::instance->GetPlaybackPositionMS(SongManager::instance->m_activeSong->m_audioChannelHandle)) / 1000.0f;
+        sampleRate = static_cast<float>(SongManager::instance->m_activeSong->m_samplerate);
+    }
+    float iChannelTime[4] = { 0.0f, songTime, 0.0f, 0.0f };
+
+    //Calculate date
+    std::time_t timeNow = std::time(nullptr);
+    //Supressed because localtime_s isn't actually accessible because I don't know why try for yourself. >:I
+#pragma warning(suppress: 4996)
+    std::tm time = *std::localtime(&timeNow);
+
     program->SetFloatUniform("iTime", static_cast<float>(GetCurrentTimeSeconds()));
     program->SetFloatUniform("iTimeDelta", deltaSeconds);
     program->SetFloatUniform("iFrame", static_cast<float>(GetFrameNumber() - program->m_frameCreated));
     program->SetFloatUniform("iFrameRate", GetCurrentFrameRate());
+    program->SetFloatUniform("iChannelTime", iChannelTime, 4);
     program->SetVec4Uniform("iMouse", Vector4(currentPos, dragPos));
+    program->SetVec4Uniform("iDate", Vector4(static_cast<float>(time.tm_year - 1), static_cast<float>(time.tm_mon - 1), static_cast<float>(time.tm_mday), static_cast<float>(time.tm_sec))); //TODO: include fractional part of sec.
+    program->SetFloatUniform("iSampleRate", sampleRate);
+
 }
